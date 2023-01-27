78b3452bbda4f2f7ae141ccb4cec69fb31b6052b
kamino <nkono.mic@gmail.com>
2023-01-27 23:59:24 +0300

    single event

--- pages/events/[cat]/index.js
+++ pages/events/[cat]/index.js
@@ -1,4 +1,4 @@
-
+import { CatEvent } from "../../../src/components/events/catEvent";
 const EventsCatPage = ({data, pageName}) => <CatEvent data={data} pageName={pageName} />
 
 export default EventsCatPage;
